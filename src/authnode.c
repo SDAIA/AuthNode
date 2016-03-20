3e53bf300aaf03cfbe55696dd01cfcb0c7843dac
Ángel González <aglezabad@gmail.com>
2016-03-20 20:26:32 +0100

    Added new functions to AuthNode

--- src/authnode.c
+++ src/authnode.c
@@ -8,6 +8,17 @@ int auth_node_init() {
 int auth_node_ping(struct http_request *req) {
     char * pong = auth_node_json_pong();
     http_response(req, HTTP_STATUS_OK, pong, (unsigned)strlen(pong));
-    kore_log(LOG_NOTICE, "[%s] %s -> %d", kore_http_pretty_method(req->method), req->path, HTTP_STATUS_OK);
+    kore_log(LOG_NOTICE, "[%s] %s -> %d", kore_http_pretty_method(req->method),
+        req->path, HTTP_STATUS_OK);
     return (KORE_RESULT_OK);
+}
+
+int auth_node_login(struct http_request *req) {
+    kore_log(LOG_DEBUG, "[AuthNode] Login not implemented");
+    return (KORE_RESULT_ERROR);
+}
+
+int auth_node_logout(struct http_request *req) {
+    kore_log(LOG_DEBUG, "[AuthNode] Logout not implemented");
+    return (KORE_RESULT_ERROR);
 }
--- src/includes/authnode.h
+++ src/includes/authnode.h
@@ -3,7 +3,9 @@
 #include <kore/kore.h>
 #include <kore/http.h>
 #include <kore/httppretty.h>
+#include <libconfig.h>
 #include "includes/jsonstr.h"
+#include "includes/structs.h"
 
 #define AUTH_NODE_H_
 
@@ -12,8 +14,12 @@
 #define RET_NOT_IMPLEMENTED -1
 #define RET_ERR -2
 
+extern struct db_params database_params;
 
 int auth_node_init(void);
 int auth_node_ping(struct http_request *);
+int auth_node_login(struct http_request *);
+int auth_node_logout(struct http_request *);
+
 
 #endif
